2fefff9eb2069ca66795874119ad850beae8900b
Cynthia <cynthiadidier8@gmail.com>
2026-06-28 12:57:33 +0200

    Basic and ugly menu for level manager

--- mirror-onyxia/level_manager.gd
+++ mirror-onyxia/level_manager.gd
@@ -6,6 +6,7 @@ var levels_unlocked: int = 1
 func complete_level() -> void:
 	if current_level >= levels_unlocked:
 		levels_unlocked += 1
+	WorldManager.current_world = 0b00
 	go_to_next_level()
 	
 func go_to_next_level() -> void:
@@ -15,4 +16,8 @@ func go_to_next_level() -> void:
 func go_to_level(level: int) -> void:
 	if level <= levels_unlocked:
 		current_level = level
+		WorldManager.current_world = 0b00
 		get_tree().change_scene_to_file("res://scene/platformer/platformer_level_" + str(level) + ".tscn")
+
+func go_to_menu() -> void:
+	get_tree().change_scene_to_file("res://scene/menu.tscn")
