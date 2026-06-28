7aa97e8b64ba05e74f6146675a30a38377ed6c2a
Cynthia <cynthiadidier8@gmail.com>
2026-06-28 12:10:56 +0200

    Add level exit

---
+++ mirror-onyxia/level_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var current_level: int = 1
+var levels_unlocked: int = 1
+
+func complete_level() -> void:
+	if current_level >= levels_unlocked:
+		levels_unlocked += 1
+	go_to_next_level()
+	
+func go_to_next_level() -> void:
+	current_level += 1
+	get_tree().change_scene_to_file("res://scene/platformer/platformer_level_" + str(current_level) + ".tscn")
+	
+func go_to_level(level: int) -> void:
+	if level <= levels_unlocked:
+		current_level = level
+		get_tree().change_scene_to_file("res://scene/platformer/platformer_level_" + str(level) + ".tscn")
--- mirror-onyxia/script/platformer/onyxia_platformer.gd
+++ mirror-onyxia/script/platformer/onyxia_platformer.gd
@@ -5,6 +5,9 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+func _ready() -> void:
+	add_to_group("player")
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
