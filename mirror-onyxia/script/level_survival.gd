6a1970c3aa7c874a1d7d33561727b6e1ba87bec7
parusthu <pierre-alexandre.rusthul@edu.univ-fcomte.fr>
2026-06-29 20:00:30 +0200

    timer

--- mirror-onyxia/script/level_survival.gd
+++ mirror-onyxia/script/level_survival.gd
@@ -9,14 +9,22 @@ class_name LevelSurvival;
 
 @export var maxSpawn : float;
 
+@export var timeWin : float;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var inst_spawner = spawner.instantiate();
 	inst_spawner.vtsPhase = vtsPhase;
 	inst_spawner.minSpawn = minSpawn;
 	inst_spawner.maxSpawn = maxSpawn;
-
+	add_child(inst_spawner);
+	$TimeWin.wait_time = timeWin;
+	$TimeWin.start();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$Countdown.text = str($TimeWin.time_left).pad_decimals(0);
+
+
+func _on_time_win_timeout() -> void:
 	pass
