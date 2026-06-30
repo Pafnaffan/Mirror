81cf93b3cf5c02e4ae54cce08703477ba41ac8ce
Cynthia <cynthiadidier8@gmail.com>
2026-06-30 14:23:23 +0200

    MUSICA

--- mirror-onyxia/audio_manager.gd
+++ mirror-onyxia/audio_manager.gd
@@ -1,10 +1,47 @@
 extends Node
 
 @onready var music_player: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var menu_player: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var level_player: AudioStreamPlayer = AudioStreamPlayer.new()
+
+var fade_tween: Tween
 
 func _ready() -> void:
 	add_child(music_player)
 	music_player.bus = "Music"
+	
+	add_child(menu_player)
+	add_child(level_player)
+	menu_player.stream = preload("res://sound/menu_music.mp3")
+	level_player.stream = preload("res://sound/platformer_music.mp3")
+	menu_player.volume_db = 0
+	level_player.volume_db = -80
+	menu_player.play()
+	level_player.play()
+	
+func fade_to_level() -> void:
+	if fade_tween:
+		fade_tween.kill()
+	fade_tween = create_tween()
+	fade_tween.set_parallel(true)
+	#fade_tween.tween_property(menu_player, "volume_db", -80, 1)
+	#fade_tween.tween_property(level_player, "volume_db", 0, 1)
+	fade_tween.tween_method(_set_crossfade_progress, 0.0, 1.0, 1)
+	
+func _set_crossfade_progress(t: float) -> void:
+	var menu_vol = cos(t * PI / 2.0)
+	var level_vol = sin(t * PI / 2.0)
+	menu_player.volume_db = linear_to_db(menu_vol)
+	level_player.volume_db = linear_to_db(level_vol)
+	
+func fade_to_menu() -> void:
+	if fade_tween:
+		fade_tween.kill()
+	fade_tween = create_tween()
+	fade_tween.set_parallel(true)
+	#fade_tween.tween_property(menu_player, "volume_db", 0, 0.5)
+	#fade_tween.tween_property(level_player, "volume_db", -80, 0.5)
+	fade_tween.tween_method(_set_crossfade_progress, 1.0, 0.0, 1)
 
 func play_music(stream: AudioStream, loop: bool = true) -> void:
 	music_player.stream = stream
--- mirror-onyxia/script/menu.gd
+++ mirror-onyxia/script/menu.gd
@@ -10,4 +10,8 @@ func _ready() -> void:
 	for i in buttons.size():
 		var level = i + 1
 		buttons[i].disabled = level > LevelManager.levels_unlocked
-		buttons[i].pressed.connect(func(): LevelManager.go_to_level(level))
+		buttons[i].pressed.connect(func():
+			AudioManager.fade_to_level()
+			await get_tree().create_timer(0.5).timeout
+			LevelManager.go_to_level(level)
+		)
